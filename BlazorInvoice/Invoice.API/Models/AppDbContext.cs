e4e5faf88454d7379cd2ad7d092b441f734602a8
AnkitSahay <ankitsahay1990@gmail.com>
2023-01-21 11:20:40 +0530

    Added DB Context class DBSets

---
+++ BlazorInvoice/Invoice.API/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Invoice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invoice.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Invoice.Models.Invoice> Invoices { get; set; }
+        public DbSet<Invoice.Models.InvoiceItem> invoiceItems { get; set; }
+        public DbSet<Invoice.Models.Customer> Customers { get; set; }
+        public DbSet<Invoice.Models.Merchant> Merchants { get; set; }
+        public DbSet<Invoice.Models.BankAccountInfo> BankAccountInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
