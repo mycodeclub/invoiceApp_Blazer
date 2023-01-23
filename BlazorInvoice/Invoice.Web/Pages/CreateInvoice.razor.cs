6cae62c939752b3d97bfa0eab3f3194454f32293
AnkitSahay <ankitsahay1990@gmail.com>
2023-01-23 10:34:58 +0530

    Add Invoice Items on UI

--- BlazorInvoice/Invoice.Web/Pages/CreateInvoice.razor.cs
+++ BlazorInvoice/Invoice.Web/Pages/CreateInvoice.razor.cs
@@ -3,15 +3,26 @@
     public partial class CreateInvoice
     {
         private static Invoice.Models.Invoice? _invoice { get; set; }
-        static CreateInvoice() {
+        static CreateInvoice()
+        {
             _invoice ??= new Invoice.Models.Invoice()
             {
                 CustomerInfo = new Models.Customer() { },
-                Items = new List<Models.InvoiceItem>() { new Models.InvoiceItem() { ProductServiceDescription="CoCo", UnitCounts=8,UnitPrice=12} },
+                Items = new List<Models.InvoiceItem>() { new Models.InvoiceItem() { ProductServiceDescription = "CoCo", UnitCounts = 8, UnitPrice = 12 } },
                 Payments = new List<Models.InvoicePayment>() { },
             };
         }
-        public  CreateInvoice()
+        private void AddNewItem()
+        {
+            if (_invoice == null)
+                _invoice = new Models.Invoice() { };
+            if (_invoice.Items == null)
+                _invoice.Items = new List<Models.InvoiceItem>() { };
+            _invoice.Items.Add(new Models.InvoiceItem() { });
+        }
+
+
+        public CreateInvoice()
         {
 
 
